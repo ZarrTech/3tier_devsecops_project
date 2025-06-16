e5683b46830ab7c991146879d4a983aaad93f2bc
Zarrtech <prosperzarus@gmail.com>
2025-06-16 12:40:47 +0100

    aws command line instead of path

--- provider.tf
+++ provider.tf
@@ -26,7 +26,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "/usr/local/bin/aws"
+    command     = "/aws"
     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
@@ -38,7 +38,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "/usr/local/bin/aws"
+      command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
   }
